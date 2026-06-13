8e413ac121d45a0de9b39a5fb35d3ae4feaf8117
J.C.TW <cch1104@hotmail.com>
2026-06-14 02:16:03 +0800

    feature/I2C
    1.setting PB8 as I2C1_SCL, PB9 as I2C1_SDA, PA5 as GPIO_OUTPUT
    2.setting I2C.Mode:I2C
    2.1 Parameter: I2C mode:stand mode
    2.2 I2C Speed Frquency:100kHz
    3.edit I2C code same as Project I2C EEPROM

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -22,11 +22,13 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "lcd.h"
+#include "string.h"
+#define LED GPIO_PIN_5
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-
+char I2C_ADDRESS = 0xA0;
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
@@ -40,6 +42,7 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
 
 /* USER CODE BEGIN PV */
 
@@ -48,13 +51,30 @@
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+void WRITE(uint16_t MemLoc, uint8_t *pData, uint16_t len){
+	uint8_t data[5];
 
+	data[0]=(uint8_t) ((MemLoc & 0xFF00) >> 8);
+	data[1]=(uint8_t) (MemLoc & 0xFF);
+	memcpy(data+2, pData, len);
+	HAL_I2C_Master_Transmit(&hi2c1, I2C_ADDRESS, data, len+2, HAL_MAX_DELAY);
+	while(HAL_I2C_Master_Transmit(&hi2c1, I2C_ADDRESS, 0, 0, HAL_MAX_DELAY) != HAL_OK);
+}
+
+void READ(uint16_t MemLoc, uint8_t *pData, uint16_t len){
+	uint8_t addr[2];
+	addr[0] =(uint8_t) ((MemLoc & 0xFF00) >> 8);
+	addr[1] =(uint8_t) (MemLoc & 0XFF);
+	HAL_I2C_Master_Transmit(&hi2c1, I2C_ADDRESS, addr, 2, HAL_MAX_DELAY);
+	HAL_I2C_Master_Receive(&hi2c1, I2C_ADDRESS, pData, len, HAL_MAX_DELAY);
+}
 /* USER CODE END 0 */
 
 /**
@@ -65,7 +85,9 @@ int main(void)
 {
 
   /* USER CODE BEGIN 1 */
-
+  int count, i;
+  char wmsg[]={'1','5'};
+  char rmsg[10];
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -86,10 +108,25 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
+ //=========================LCD=================================================//
   lcd_Init();
-  lcd_Puts("Nucleo-L476 HW3");
+  lcd_Puts("Nucleo-L476 002");
+//  lcd_Clear();
+//==========================LCD END=============================================//
+//==========================I2C=================================================//
+  HAL_I2C_Init(&hi2c1);
+  WRITE(0x1000, (uint8_t*)wmsg, strlen(wmsg)+1);
+  HAL_Delay(5000);
+  READ(0x1000, (uint8_t*)rmsg, strlen(rmsg)+1);
+  count= 10*(rmsg[0] - '0')+ rmsg[1]-'0';
 
+  for(i=0; i<count; i++){
+	  HAL_GPIO_TogglePin(GPIOA, LED);
+	  HAL_Delay(1000);
+  }
+//==========================I2C END=================================================//
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -153,6 +190,54 @@ void SystemClock_Config(void)
 }
 
 /**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.Timing = 0x10D19CE4;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Analogue filter
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Digital filter
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
@@ -166,24 +251,19 @@ static void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9
-                          |GPIO_PIN_10|GPIO_PIN_11, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8
+                          |GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : PA6 PA7 PA8 PA9
-                           PA10 PA11 */
-  GPIO_InitStruct.Pin = GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9
-                          |GPIO_PIN_10|GPIO_PIN_11;
+  /*Configure GPIO pins : PA5 PA6 PA7 PA8
+                           PA9 PA10 PA11 */
+  GPIO_InitStruct.Pin = GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8
+                          |GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : PA13 */
-  GPIO_InitStruct.Pin = GPIO_PIN_13;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
